6946d6886b213282de31932ca21b64e7bffb445b
ihorlapka <tailihor92@gmail.com>
2026-01-09 18:55:20 +0200

    ignore SchemaCompatibilityTest for auto run

--- src/test/java/com/iot/devices/management/telemetry_event_processor/SchemaCompatibilityTest.java
+++ src/test/java/com/iot/devices/management/telemetry_event_processor/SchemaCompatibilityTest.java
@@ -7,10 +7,12 @@ import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.Schema;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+@Ignore
 @Slf4j
 public class SchemaCompatibilityTest {
 
